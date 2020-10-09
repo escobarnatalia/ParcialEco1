948f5bd3caa5bdde9ec90ba73fbc7bb7e9d06c11
escobarnatalia <60725004+escobarnatalia@users.noreply.github.com>
2020-10-09 03:27:28 -0500

    Listo botones

---
+++ app/src/main/java/co/natalia/parcialeco1/model/Nombre.java
@@ -0,0 +1,20 @@
+package co.natalia.parcialeco1.model;
+
+public class Nombre {
+    private String nombre;
+
+    public Nombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Nombre() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
